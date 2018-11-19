9b562682b96e4d233c2d4b14c80004555a24abad
stennant <stennant@scottlogic.com>
2018-11-19 11:48:56 +0000

    Added tests for single partition with decisions

---
+++ generator/resources/partitioning-tests/no-partitions-with-decisions/README.md
@@ -0,0 +1,11 @@
+Tests that a tree that contains decisions that all have relationships returns a single tree from the partitioner
+
+Profile Fields:
+    ProductType
+    Price
+    Category
+    MaximumAmount
+    AuditType
+
+Expected Trees:
+    1(ProductType, Price, Category, MaximumAmount, AuditType)
--- generator/src/test/java/com/scottlogic/deg/generator/decisiontree/test_utils/ConstraintDto.java
+++ generator/src/test/java/com/scottlogic/deg/generator/decisiontree/test_utils/ConstraintDto.java
@@ -14,7 +14,8 @@ import com.scottlogic.deg.generator.decisiontree.tree_partitioning.test_utils.ma
         @JsonSubTypes.Type(value = NotConstraintDto.class, name = "NotConstraint"),
         @JsonSubTypes.Type(value = IsNullConstraintDto.class, name = "IsNullConstraint"),
         @JsonSubTypes.Type(value = IsLessThanConstantConstraintDto.class, name = "IsLessThanConstantConstraint"),
-        @JsonSubTypes.Type(value = IsGreaterThanConstantConstraintDto.class, name = "IsGreaterThanConstantConstraint")
+        @JsonSubTypes.Type(value = IsGreaterThanConstantConstraintDto.class, name = "IsGreaterThanConstantConstraint"),
+        @JsonSubTypes.Type(value = IsGreaterThanOrEqualToConstantConstraintDto.class, name = "IsGreaterThanOrEqualToConstantConstraint")
 })
 public interface ConstraintDto extends IConstraintMapper {
 }
