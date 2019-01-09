06839189cd228ff7c3b6469675bb9f2702dd0eca
Frances Bromley <fbromley@scottlogic.co.uk>
2019-01-09 10:23:00 +0000

    NoTypeRestriction Tests begginning of

---
+++ generator/src/test/java/com/scottlogic/deg/generator/restrictions/NoTypeRestrictionTests.java
@@ -0,0 +1,14 @@
+package com.scottlogic.deg.generator.restrictions;
+
+import org.junit.jupiter.api.Test;
+
+public class NoTypeRestrictionTests {
+    @Test
+    void equals_whenOtherObjectIsNull_returnsFalse() {
+        NoTypeRestriction restriction = new NoTypeRestriction();
+
+        res
+
+    }
+
+}
