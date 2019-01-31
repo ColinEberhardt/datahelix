f193e7a3e9fb0abe095f185c77ab121e2488abbe
Simon Laing <slaing@scottlogic.com>
2019-01-31 16:39:52 +0000

    Tidy-up, removal of imported packages

--- generator/src/main/java/com/scottlogic/deg/generator/fieldspecs/ReductiveRowSpec.java
+++ generator/src/main/java/com/scottlogic/deg/generator/fieldspecs/ReductiveRowSpec.java
@@ -2,16 +2,8 @@ package com.scottlogic.deg.generator.fieldspecs;
 
 import com.scottlogic.deg.generator.Field;
 import com.scottlogic.deg.generator.ProfileFields;
-import com.scottlogic.deg.generator.generation.FieldSpecValueGenerator;
-import com.scottlogic.deg.generator.generation.GenerationConfig;
-import com.scottlogic.deg.generator.generation.databags.DataBag;
-import com.scottlogic.deg.generator.generation.databags.DataBagSource;
-import com.scottlogic.deg.generator.generation.databags.MultiplexingDataBagSource;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class ReductiveRowSpec extends RowSpec {
     public final Field lastFixedField;
